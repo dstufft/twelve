ac9a42c4cfcb45a2731851ab106c2f36de73836b
Donald Stufft <donald.stufft@gmail.com>
2012-06-14 04:52:02 -0400

    Remove Python 3.x claims from setup.py as extensions isn't 3.x

--- setup.py
+++ setup.py
@@ -44,8 +44,5 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
     ),
 )
