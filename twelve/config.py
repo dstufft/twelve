c60215983f81ddd2a576cb44676daa1ca781c199
Donald Stufft <donald.stufft@gmail.com>
2012-06-14 06:38:53 -0400

    Move to On Demand loading of data and support arbitrary env variables

--- twelve/config.py
+++ twelve/config.py
@@ -16,28 +16,40 @@ class Configuration(object):
         self.names = names
         self.values = {}
 
-        self._load_all()
-
     def __getattr__(self, name):
-        return self.values.get(name)
+        if not name in self.values:
+            names = [plugin.name for plugin in extensions.get(group="twelve.services")]
+            environ = self.environ if self.environ is not None else os.environ
+
+            if name in names:
+                # We are trying to get a Backing Service, Load it
+                self.values[name] = self._load_service(name, environ=environ)
+            elif name.upper() in environ:
+                self.values[name] = self._get_environ_value(name, environ=environ)
+            else:
+                self.values[name] = None
+
+        return self.values[name]
 
     def __repr__(self):
         return "<twelve.Configuration [{0}]>".format(",".join(self.values))
 
-    def _load_all(self):
-        # Load Services
-        self._load_services()
+    def _get_environ_value(self, name, environ):
+        value = environ.get(name.upper())
 
-    def _load_services(self):
-        import extensions
+        if self.adapter is not None:
+            adapters = list(extensions.get(group="twelve.adapters", name="{0}.{1}".format(self.adapter, name)))
+            if len(adapters):
+                adapter = adapters[0].load()
+                value = adapter(value)
 
-        for plugin in extensions.get(group="twelve.services"):
+        return value
+
+    def _load_service(self, name, environ):
+        for plugin in extensions.get(group="twelve.services", name=name):
             service = plugin.load()
 
-            value = service(
-                        self.environ if self.environ is not None else os.environ,
-                        self.names.get(plugin.name)
-                    )
+            value = service(environ, self.names.get(plugin.name))
 
             if self.adapter is not None:
                 adapters = list(extensions.get(group="twelve.adapters", name="{0}.{1}".format(self.adapter, plugin.name)))
@@ -45,4 +57,4 @@ class Configuration(object):
                     adapter = adapters[0].load()
                     value = adapter(value)
 
-            self.values[plugin.name] = value
+            return value
